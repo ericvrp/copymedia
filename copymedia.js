bec6dac9942393d4b0f5dd1821d6008d554596f3
Eric van Riet Paap <ericvrp@gmail.com>
2019-10-22 13:08:33 +0200

    WIP

--- copymedia.js
+++ copymedia.js
@@ -1,15 +1,44 @@
 const config = require('./config.json');
 
-const { execSync } = require('child_process');
+// const { execSync } = require('child_process');
+
+//
+const fs = require('fs');
+const path = require('path');
+
+const getAllFiles = dir =>
+  fs.readdirSync(dir).reduce((files, file) => {
+    const name = path.join(dir, file);
+    const isDirectory = fs.statSync(name).isDirectory();
+    return isDirectory ? [...files, ...getAllFiles(name)] : [...files, name];
+  }, []);
+
+//
+const readHistory = () => {
+    const historyJSON = fs.readdirSync('history').filter(f => path.extname(f) === '.json');
+    console.log(`TODO: read and parse ${historyJSON.length} history files`);
+    // console.log(historyJSON)
+    
+    return {
+
+    }
+}
+
+//
+const writeHistory = (history, historyFilename) => {
+    console.log(`TODO: write new history file to ${historyFilename}`);
+}
+
+//
+const history = readHistory();
 
 config.forEach(conf => {
-    const ls = `ls -lR ${conf.source} | grep -i -E "(${conf.extensions.join('|')})"`
-
-    const sourceFiles = execSync(ls).toString().trim().split('\n')
-    const historyFilename = `history/${new Date()}.json`;
+    const sourceFiles = getAllFiles(conf.source).filter(f => conf.extensions.includes(path.extname(f)));
+    // console.log(sourceFiles);
 
     console.log(`TODO: read ${sourceFiles.length} files from ${conf.source}`);
     console.log(`TODO: write ${sourceFiles.length} files to ${conf.destination}`);
-    console.log(`TODO: write ${historyFilename}`);
+
+    writeHistory({}, `history/${new Date()}.json`);
 })
 
